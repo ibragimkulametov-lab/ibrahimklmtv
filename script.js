d237059983405b407d47a3ef584e0e20026e49b7
Ibrahim <ibragim.kulametov@icloud.com>
2026-05-01 01:25:12 +0200

    Тотальное изменение дизайна

--- script.js
+++ script.js
@@ -15,12 +15,13 @@ window.addEventListener("scroll", () => {
 const translations = {
     ru: {
         title: "Ибрахим & Фериде",
+        name1: "Ибрахим",
+        name2: "Фериде",
         invite: "Приглашение на свадьбу",
         date: "Дата",
         dateValue: "10 октября 2026",
         time: "Время",
         place: "Место",
-        placeValue: "Waldkraiburg",
         ayahTitle: "Аят о любви и милосердии",
         ayahText: "«Среди Его знамений — то, что Он создал для вас жен из вас самих, чтобы вы находили в них покой, и установил между вами любовь и милость»",
         duaTitle: "Дуа за молодых",
@@ -33,12 +34,13 @@ const translations = {
 
     de: {
         title: "Ibrahim & Feride",
+        name1: "Ibrahim",
+        name2: "Feride",
         invite: "Hochzeitseinladung",
         date: "Datum",
         dateValue: "10. Oktober 2026",
         time: "Zeit",
         place: "Ort",
-        placeValue: "Waldkraiburg",
         ayahTitle: "Vers über Liebe und Barmherzigkeit",
         ayahText: "„Zu Seinen Zeichen gehört, dass Er für euch Gattinnen aus euch selbst erschuf, damit ihr bei ihnen Ruhe findet, und Er hat zwischen euch Liebe und Barmherzigkeit gesetzt.“",
         duaTitle: "Bittgebet für das Paar",
@@ -51,12 +53,13 @@ const translations = {
 
     tr: {
         title: "İbrahim & Feride",
+        name1: "İbrahim",
+        name2: "Feride",
         invite: "Düğün davetiyesi",
         date: "Tarih",
         dateValue: "10 Ekim 2026",
         time: "Saat",
         place: "Yer",
-        placeValue: "Waldkraiburg",
         ayahTitle: "Sevgi ve merhamet ayeti",
         ayahText: "“O’nun ayetlerinden biri de, size kendi cinsinizden eşler yaratması, aranızda sevgi ve merhamet var etmesidir.”",
         duaTitle: "Çift için dua",
@@ -78,6 +81,13 @@ function setLang(lang) {
                 el.innerText = translations[lang][key];
             }
         });
+const names = document.querySelectorAll(".name, .amp");
+
+names.forEach(el => {
+    el.style.animation = "none";
+    el.offsetHeight; // перезапуск анимации
+    el.style.animation = "";
+});
 
         document.body.style.opacity = "1";
     }, 200);
